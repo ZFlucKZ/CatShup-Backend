d160124072315ba89bcced7f6d7d72f82e65109e
Fluke <guyguy5050@gmail.com>
2022-11-03 21:48:47 +0700

    get user data and protect middleware

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,10 +4,13 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  getUser,
 } = require('../controllers/userController');
+const protect = require('../middleWare/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/logout', logoutUser);
+router.get('/getuser', protect, getUser);
 
 module.exports = router;
