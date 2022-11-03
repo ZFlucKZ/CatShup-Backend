4a88faa613ab13cb4c010827699edab8d6ae5844
Fluke <guyguy5050@gmail.com>
2022-11-03 16:01:37 +0700

    Register method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,48 @@
-const registerUser = (req, res) => {
-  if (!req.body.email) {
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userSchema');
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
     res.status(400);
-    throw new Error('Please add an email');
+    throw new Error('Please fill all require fields');
+  }
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error('Password must be up to 6 characters');
   }
 
-  res.send('Register User');
-};
+  // Check if user email already exist
+  const userEmailExists = await User.findOne({ email });
+  if (userEmailExists) {
+    res.status(400);
+    throw new Error('Email has already exist');
+  }
+
+  // Create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 module.exports = {
   registerUser,
