1c3585436f1fd5bb4f489bc875abb57639227389
Fluke <guyguy5050@gmail.com>
2022-11-05 00:32:20 +0700

    delete product

--- controllers/productController.js
+++ controllers/productController.js
@@ -31,6 +31,7 @@ const createProduct = asyncHandler(async (req, res) => {
       filePath: uploadedFile.secure_url,
       fileType: req.file.mimetype,
       fileSize: req.file.size,
+      fileId: uploadedFile.public_id,
     };
   }
 
@@ -58,11 +59,14 @@ const getProducts = asyncHandler(async (req, res) => {
 // Get a Product
 const getProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
+
+  // if product doesn't exist
   if (!product) {
     res.status(404);
     throw new Error('Product not found');
   }
 
+  // Match product to its user
   if (product.user.toString() !== req.user.id) {
     res.status(404);
     throw new Error('User not authorized');
@@ -71,8 +75,43 @@ const getProduct = asyncHandler(async (req, res) => {
   res.status(200).json(product);
 });
 
+// Delete a Product
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  // if product doesn't exist
+  if (!product) {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+
+  // Match product to its user
+  if (product.user.toString() !== req.user.id) {
+    res.status(404);
+    throw new Error('User not authorized');
+  }
+
+  try {
+    deletedFile = await cloudinary.uploader.destroy(product.image.fileId, {
+      resource_type: 'image',
+    });
+  } catch (error) {
+    res.status(500);
+    throw new Error('Image could not be deleted');
+  }
+  await product.remove();
+  res.status(200).json({ message: 'Product deleted' });
+});
+
+// Update Product
+const updateProduct = asyncHandler(async (req, res) => {
+  res.send('Update');
+});
+
 module.exports = {
   createProduct,
   getProducts,
   getProduct,
+  deleteProduct,
+  updateProduct,
 };
